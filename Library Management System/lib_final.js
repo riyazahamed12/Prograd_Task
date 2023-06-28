7793bcdafcf1d532818ecc18fe8b898e07436faa
riyazahamed12 <120769330+riyazahamed12@users.noreply.github.com>
2023-06-28 21:59:10 +0530

    Update lib_final.js

--- Library Management System/lib_final.js
+++ Library Management System/lib_final.js
@@ -11,7 +11,7 @@ const loginBtn = document.querySelector('.login-btn');
 
     loginBtn.addEventListener('click', () => {
       console.log('Login clicked');
-      window.location.href = 'f1.html';
+      window.location.href = './f1.html';
     });
 
     logoutBtn.addEventListener('click', () => {
